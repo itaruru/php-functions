06380dae11faad175df6066cb662cef37a0c48d1
Tarumi Tomoyuki <tarumitomoyuki@gmail.com>
2017-05-26 10:13:39 +0900

    Update functions.php

--- functions.php
+++ functions.php
@@ -144,3 +144,25 @@ function add_html_attr_quotes($html) {
 function add_html_attr_quotes_callback($matches) {
     return $matches[1] . '="' . str_replace(array('"', "'"), '', $matches[2]) . '"';
 }
+
+/**
+ * @param string $buffer
+ * @see https://stackoverflow.com/questions/6225351/how-to-minify-php-page-html-output
+ * @return mixed
+ */
+function sanitize_output($buffer) {
+    $search = array(
+        '/\>[^\S ]+/s',     // strip whitespaces after tags, except space
+        '/[^\S ]+\</s',     // strip whitespaces before tags, except space
+        '/(\s)+/s',         // shorten multiple whitespace sequences
+        '/<!--(.|\s)*?-->/' // Remove HTML comments
+    );
+    $replace = array(
+        '>',
+        '<',
+        '\\1',
+        ''
+    );
+    $buffer = preg_replace($search, $replace, $buffer);
+    return $buffer;
+}
